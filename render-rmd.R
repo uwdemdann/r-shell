b76d045d1392a4b35848c4bb7c796fbf1a3d4d87
Hekman Daniel J <DJH008@uwhis.hosp.wisc.edu>
2019-11-22 10:47:39 -0600

    Change output file

--- render-rmd.R
+++ render-rmd.R
@@ -1,3 +1,6 @@
 library(rmarkdown)
 
-render(input='surveys-report.Rmd')
+render(input='surveys-report.Rmd',
+       output_file=paste0('survey',as.character(Sys.Date(),format='%Y%m%d'),'.html'),
+       output_dir = 'reports',
+       params = list(file = 'data/plot1.csv'))
